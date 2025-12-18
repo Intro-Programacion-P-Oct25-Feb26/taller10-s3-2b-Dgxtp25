0a04470f7c0d32f6b14a31409c8e6b80becae684
Diego B. <dgxtpbr@gmail.com>
2025-12-18 11:47:43 -0500

    Problema 2 - Cambio1

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,38 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}
+        };
+
+        int totalMenores = 0;
+        int[] adultosPorFamilia = new int[edades.length];
+
+        for (int i = 0; i < edades.length; i++) {
+            int adultos = 0;
+
+            for (int j = 0; j < edades[i].length; j++) {
+                if (edades[i][j] < 18) {
+                    totalMenores++;
+                } else {
+                    adultos++;
+                }
+            }
+
+            adultosPorFamilia[i] = adultos;
+        }
+
+        System.out.println("Total de menores de edad: " + totalMenores);
+        System.out.println();
+
+        for (int i = 0; i < adultosPorFamilia.length; i++) {
+            System.out.println("Familia " + (i + 1)
+                    + " Adultos: " + adultosPorFamilia[i]);
+        }
+    }
 }
