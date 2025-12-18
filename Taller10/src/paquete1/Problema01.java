9f541e96fe5a61215445f15c4c61edad460366ac
Diego B. <dgxtpbr@gmail.com>
2025-12-18 10:56:15 -0500

    Problema 1 - Cambio 1

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,28 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+        int[] totalAsistencias = new int[estudiantes.length];
+        
+        for (int i = 0; i < asistencia.length; i++){
+            int contador = 0;
+            for (int j = 0; j < asistencia[i].length; j++){
+                if (asistencia[i][j]){
+                    contador ++;
+                } 
+            } 
+            totalAsistencias[i] = contador;
+            
+        }
+        System.out.println("Reporte de asistencia\n");
+
     }
-    
+
 }
