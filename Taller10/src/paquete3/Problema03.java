09e3294a4e9cb83b6ebee5972ae6b2aed2b7e023
Diego B. <dgxtpbr@gmail.com>
2025-12-18 12:39:28 -0500

    Problema 3 - Cambio 1

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,29 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
+    public static void main(String[] args) {
+     boolean[][] sensores = {
+    {true, true, true, true, true, true},
+    {true, false, true, false, true, false},
+    {false, false, false, false, false, false},
+    {true, true, false, true, true, false}
+};
+     int zonasSinFallos = 0;
+     for (int i = 0; i < sensores.length; i++){
+         boolean hayFallo = false;
+     for (int j = 0; j < sensores[i].length; j++){    
+         if (!sensores[i][j]){
+             hayFallo = true;
+             
+         }
+     }
+     if (!hayFallo){
+         zonasSinFallos++;
+     }else{
+         System.out.println("La zona " + (i + 1) + " presenta al menos un fallo");
+     }
+     }
+      System.out.println("\nZonas que funcionan sin fallos " + zonasSinFallos); 
+    }
     
 }
