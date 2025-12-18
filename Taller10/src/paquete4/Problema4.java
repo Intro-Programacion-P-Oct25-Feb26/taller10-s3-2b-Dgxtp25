d744ae203bc4b7412d61227ca664809dcfbf8dc7
Diego B. <dgxtpbr@gmail.com>
2025-12-18 14:36:41 -0500

    Problema 4 y Problema 5

---
+++ Taller10/src/paquete4/Problema4.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete4;
+
+/**
+ *
+ * @author reroes
+ */
+
+import java.util.Scanner;
+
+public class Problema4 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int[][] A = new int[3][2];
+        int[][] B = new int[3][2];
+
+        // Leer matriz A
+        System.out.println("Ingrese los valores de la matriz A:");
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                System.out.print("A[" + i + "][" + j + "]: ");
+                A[i][j] = entrada.nextInt();
+            }
+        }
+
+        // Leer matriz B
+        System.out.println("\nIngrese los valores de la matriz B:");
+        for (int i = 0; i < B.length; i++) {
+            for (int j = 0; j < B[i].length; j++) {
+                System.out.print("B[" + i + "][" + j + "]: ");
+                B[i][j] = entrada.nextInt();
+            }
+        }
+
+        // Mostrar matriz A
+        System.out.println("\nMatriz A:");
+        for (int[] A1 : A) {
+            for (int j = 0; j < A1.length; j++) {
+                System.out.print(A1[j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Mostrar matriz B
+        System.out.println("\nMatriz B:");
+        for (int[] B1 : B) {
+            for (int j = 0; j < B1.length; j++) {
+                System.out.print(B1[j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Comparación de matrices
+        boolean cumpleCondicion = true;
+        boolean existeMayor = false;
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (A[i][j] < B[i][j]) {
+                    cumpleCondicion = false;
+                }
+                if (A[i][j] > B[i][j]) {
+                    existeMayor = true;
+                }
+            }
+        }
+
+        // Resultado final
+        if (cumpleCondicion && existeMayor) {
+            System.out.println("\nLa matriz A es mayor que la matriz B");
+        } else {
+            System.out.println("\nLa matriz A no es mayor que la matriz B");
+        }
+    }
+}
--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,32 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+
+        String[][] estudiantes = {
+            {"Sarah Ward", "Philip Payne"},
+            {"Carrie Burton", "Lauren Rice"},
+            {"Emma Escobar", "Lori Flores"},
+            {"Steven West", "Toni Martin"}
+        };
+
+        String acumulador = "";
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+
+                char inicial = estudiantes[i][j].charAt(0);
+
+                if (inicial == 'S' || inicial == 'P' || inicial == 'T') {
+                    acumulador += estudiantes[i][j] + "\n";
+                }
+            }
+        }
+
+        System.out.println("Estudiantes con inicial S, P o T:");
+        System.out.println(acumulador);
+    }
 }
+
+
